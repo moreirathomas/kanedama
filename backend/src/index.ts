03031f05bfbd9655817951f540c15cabbb24a0ca
moreirathomas <moreirathomas97@gmail.com>
2022-11-15 11:33:05 +0100

    chore:  pass port as env var

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,24 +4,23 @@ import { connect } from './database';
 import { getEnv, getEnvInteger, getEnvArray } from './env';
 import { start } from './server';
 
-const PORT = 3000;
-
-const config: Knex.Config = {
-  client: getEnv('DB_CLIENT'),
-  connection: {
-    host: getEnv('DB_HOST'),
-    port: getEnvInteger('DB_PORT'),
-    user: getEnv('DB_USER'),
-    password: getEnv('DB_PASSWORD'),
-  },
-  searchPath: getEnvArray('DB_SEARCH_PATH'),
-};
-
 async function main() {
+  const PORT = getEnvInteger('PORT');
+
+  const config: Knex.Config = {
+    client: getEnv('DB_CLIENT'),
+    connection: {
+      host: getEnv('DB_HOST'),
+      port: getEnvInteger('DB_PORT'),
+      user: getEnv('DB_USER'),
+      password: getEnv('DB_PASSWORD'),
+    },
+    searchPath: getEnvArray('DB_SEARCH_PATH'),
+  };
+
   const pg = connect(config);
 
-  const address = await start({ port: PORT, logger: true }, pg);
-  console.log(`Server listening on ${address} 🚀`);
+  await start({ port: PORT, logger: true }, pg);
 }
 
 main().catch(console.error);
