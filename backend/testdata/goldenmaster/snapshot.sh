1a0cf53d8a5d3705fd134861b89420451ed7339b
moreirathomas <moreirathomas97@gmail.com>
2022-11-15 16:09:56 +0100

    chore: remove bash e2e testing

--- backend/testdata/goldenmaster/snapshot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -s "http://localhost:3000/registration?name=alice&email=alice@hey.com&password=1234" \
-    > "./testdata/goldenmaster/registration.txt"
-
-curl -s "http://localhost:3000/login?email=alice@hey.com&password=1234" \
-    > "./testdata/goldenmaster/login.txt"
